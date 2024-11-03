60e513d7a5664716176a63b3c6fc06267c2d1260
ujjawal7748 <102591686+ujjawal7748@users.noreply.github.com>
2024-11-03 15:05:16 +0000

    Dattypes and type of different datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,34 @@
+"Use Strict" // This declares that we will now use new method of writing js code
+
+//alert(3+3);  // we cannot use alert function like this as we are using node not the browser
+
+// we must follow code entendation always
+// console.log(3+3) console.log("hello")  This will show error
+
+// now this will not show an error
+console.log(3+3) 
+console.log("hello")
+
+// Primitive Data Types
+let name="Raj"; //String data types
+let age=56;  // integer data types
+let isLoggedIn=true;  // boolean data types
+
+// Big int -> which will store very high number.
+//         -> Always prefer to write a string in double quote. 
+//         -> Boolean contains either true or false value.
+
+
+// null -> Rrepresentation of empty value
+// undefined -> varibale is declared but value is not assigned to variable.
+
+//Object
+
+console.log(typeof "Raj");
+console.log(typeof("Raj"));
+
+//Type of null is an object
+console.log(typeof(null));
+
+// Type of undefined is undefined
+console.log(typeof(undefined));
